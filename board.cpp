c204e9438fca48244d7b3e853a885a3b1a6bef91
Dominik <101473446+b14ucky@users.noreply.github.com>
2023-12-11 14:14:11 +0100

    fix: board is now generated properlly so it can be solved

--- board.cpp
+++ board.cpp
@@ -3,8 +3,12 @@
 
 #include "board.h"
 #include "validate.h"
+#include "functions.h"
 
 using namespace std;
+
+Functions functions;
+Validate validate;
 
 Board::Board()
 {
@@ -41,18 +45,96 @@ void Board::printBoard()
     }
 }
 
-void Board::generateBoard()
+void Board::fillDiagonal()
 {
-    Validate validate;
-    int tab[13] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
-    for (int i = 0; i < 9; i++)
+    for (int i = 0; i < 9; i += 3)
     {
-        for (int j = 0; j < 9; j++)
+        for (int j = i; j < 3 + i; j++)
         {
-            do
+            for (int k = i; k < 3 + i; k++)
             {
-                board[i][j] = tab[rand() % 13];
-            } while (!validate.checkBoard(board));
+                do
+                {
+                    board[j][k] = rand() % 9 + 1;
+                } while (!validate.checkSquare(board, i, i));
+            }
         }
     }
+}
+
+bool Board::fillRemaining(int i, int j)
+{
+    if (j >= 9 && i < 9 - 1)
+    {
+        i++;
+        j = 0;
+    }
+    if (i >= 9 && j >= 9)
+    {
+        return true;
+    }
+    if (i < 3)
+    {
+        if (j < 3)
+        {
+            j = 3;
+        }
+    }
+    else if (i < 6)
+    {
+        if (j == (int)(i / 3) * 3)
+        {
+            j += 3;
+        }
+    }
+    else
+    {
+        if (j == 6)
+        {
+            i++;
+            j = 0;
+            if (i >= 9)
+            {
+                return true;
+            }
+        }
+    }
+    for (int num = 1; num <= 9; num++)
+    {
+        board[i][j] = num;
+        if (validate.checkBoard(board))
+        {
+            if (fillRemaining(i, j + 1))
+            {
+                return true;
+            }
+        }
+        board[i][j] = 0;
+    }
+    return false;
+}
+
+void Board::removeFields()
+{
+    int count = 0;
+    while (count < 40)
+    {
+        int row = rand() % 9;
+        int column = rand() % 9;
+        if (board[row][column] != 0)
+        {
+            count++;
+            board[row][column] = 0;
+        }
+    }
+}
+
+void Board::generateBoard()
+{
+    // generate diagonall boxes
+    fillDiagonal();
+    // generate rest of the board
+    fillRemaining(0, 3);
+    // remove some fields from the board to make it playable
+    removeFields();
 }
--- board.h
+++ board.h
@@ -4,6 +4,9 @@ public:
     int board[9][9]{};
     void printBoard();
     void generateBoard();
+    void fillDiagonal();
+    bool fillRemaining(int i, int j);
+    void removeFields();
 
     Board();
 };
